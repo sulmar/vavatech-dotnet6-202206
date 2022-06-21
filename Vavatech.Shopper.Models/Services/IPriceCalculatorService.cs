df5ad2e7398b1dfa8f90de204829e8488dbbaf67
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-06-21 08:42:35 +0200

    Utworzenie usługi

---
+++ Vavatech.Shopper.Models/Services/IPriceCalculatorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vavatech.Shopper.Domain.Services
+{
+    public interface IPriceCalculatorService
+    {
+        decimal CalculatePrice(int productId, int customerId);
+    }
+}
