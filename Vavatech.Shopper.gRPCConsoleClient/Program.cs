4978ad5ec3647a6d9bcfc98a2b2e5fc840e57e20
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-06-23 13:02:25 +0200

    Dodanie usługi gRPC

---
+++ Vavatech.Shopper.gRPCConsoleClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using Bogus;
+using Grpc.Net.Client;
+using Vavatech.Shopper.gRPCService;
+
+Console.WriteLine("Hello, gRPC Client!");
+
+const string url = " https://localhost:7093";
+
+var channel = GrpcChannel.ForAddress(url);
+
+var client = new DeliveryService.DeliveryServiceClient(channel);
+
+// var request = new ConfirmDeliveryRequest { DeliveryId = 1, IsShipped = true };
+
+var requests = new Faker<ConfirmDeliveryRequest>()
+    .RuleFor(p => p.DeliveryId, f => f.Random.Int(1))
+    .RuleFor(p=>p.IsShipped, f=>f.Random.Bool(0.9f))
+    .GenerateForever();
+
+foreach (var request in requests)
+{
+    Console.WriteLine($"Sending {request.DeliveryId} {request.IsShipped}");
+    var response = await client.ConfirmDeliveryAsync(request);
+    Console.WriteLine($"Sent. {response.IsConfirmed}");
+
+  await Task.Delay(TimeSpan.FromSeconds(1));
+}
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
+
+
+
+
---
+++ Vavatech.Shopper.gRPCService/Services/ShopperDeliveryService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+
+namespace Vavatech.Shopper.gRPCService.Services
+{
+    public class ShopperDeliveryService : Vavatech.Shopper.gRPCService.DeliveryService.DeliveryServiceBase
+    {
+        private readonly ILogger<ShopperDeliveryService> logger;
+
+        public ShopperDeliveryService(ILogger<ShopperDeliveryService> logger)
+        {
+            this.logger = logger;
+        }
+
+        public override Task<ConfirmDeliveryResponse> ConfirmDelivery(ConfirmDeliveryRequest request, ServerCallContext context)
+        {
+            logger.LogInformation("{DeliveryId} {IsShipped}", request.DeliveryId, request.IsShipped);
+
+            var response = new ConfirmDeliveryResponse { IsConfirmed = true };
+
+            return Task.FromResult(response);
+
+        }
+    }
+}
